9fe58486baa3bfade38c9a55f6f8054a190e2819
zaycation.dev <isaiahthomas098@gmail.com>
2020-10-29 20:09:51 -0700

    redux persist store

--- redux/configureStore.js
+++ redux/configureStore.js
@@ -5,19 +5,29 @@ import { campsites } from "./campsites";
 import { comments } from "./comments";
 import { promotions } from "./promotions";
 import { partners } from "./partners";
-import { favorites } from './favorites';
+import { favorites } from "./favorites";
+import { persistStore, persistCombineReducers } from "redux-persist";
+import storage from "redux-persist/es/storage";
+
+const config = {
+  key: "root",
+  storage,
+  debug: true,
+};
 
 export const ConfigureStore = () => {
   const store = createStore(
-    combineReducers({
+    persistCombineReducers(config, {
       campsites,
       comments,
       partners,
       promotions,
-      favorites
-  }),
+      favorites,
+    }),
     applyMiddleware(thunk, logger)
   );
 
-  return store;
+  const persistor = persistStore(store);
+
+  return { persistor, store };
 };
